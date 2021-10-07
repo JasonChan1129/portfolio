2991e61edeca0a428404f40772e1c8805885e46d
Jason Chan <jasonchan19981017@hotmail.com>
2021-10-07 21:14:26 +0800

    add smooth scroll

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const welcome = document.querySelector("#welcome-section")
-let navBar = document.getElementById("navbar")
+const welcome = document.querySelector('#welcome-section');
+let navBar = document.getElementById('navbar');
 
-const options = {threshold: 0.1};
+const options = { threshold: 0.1 };
 
-const observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-           navBar.style.display = 'flex'
-        }
-        else {
-            navBar.style.display = 'none'
-        }
-    });
+const observer = new IntersectionObserver(function (entries, observer) {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			navBar.style.display = 'flex';
+		} else {
+			navBar.style.display = 'none';
+		}
+	});
 }, options);
 
 observer.observe(welcome);
