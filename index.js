237b45a9a60bd36eb8561ba341e4fb42bf1b535c
Jason Chan <jasonchan19981017@hotmail.com>
2021-10-15 20:43:44 +0800

    testing lazyloading

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ const observer = new IntersectionObserver(function (entries, observer) {
 }, options);
 
 observer.observe(welcome);
+
+const project = document.querySelector('#projects');
+const img = document.querySelectorAll('img');
+
+const options2 = { threshold: 0.5 };
+
+const observer2 = new IntersectionObserver(function (entries, observer2) {
+	entries.forEach(entry => {
+		if (entry && entry.isIntersecting) {
+			img.forEach(img => {
+				img.src = img.getAttribute('data-src');
+				img.style.animationPlayState = 'running';
+			});
+		}
+	});
+}, options2);
+
+observer2.observe(project);
